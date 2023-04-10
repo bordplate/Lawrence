8f4389610807159003bd7ebfc730740a569507d9
bordplate <vetle@bordplate.no>
2023-04-10 13:42:35 +0200

    Adds improved logging class that also writes to local file. Has not been tested for performance, and is still missing functionality.

--- Lawrence/Logger.cs
+++ Lawrence/Logger.cs
@@ -1,49 +1,68 @@
 ﻿using System;
-namespace Lawrence
-{
-	public class Logger
-	{
-		public enum Priority
-		{
-			Log = 0,
-			Error = 1,
-			Trace = 2
-		}
+using System.IO;
 
-		static Logger shared;
+namespace Lawrence {
+    public class Logger {
+        public enum Priority {
+            Log = 0,
+            Error = 1,
+            Trace = 2
+        }
 
-		string logFile;
+        private static Logger shared;
+        private readonly object _syncLock = new object();
 
+        private string _logFile;
 
+        private Logger() {
+        }
 
-		public Logger()
-		{
-		}
+        private void Log(Priority priority, string value, Exception exception = null) {
+            string timestamp = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}";
+            string logEntry = $"{timestamp} [{priority}] {value}";
 
-		void Log(Priority priority, string value)
-		{
+            lock (_syncLock) {
+                // Write to file
+                using (var streamWriter = new StreamWriter(_logFile, true)) {
+                    streamWriter.WriteLine(logEntry);
 
-		}
+                    if (exception != null) {
+                        streamWriter.WriteLine($"Exception: {exception.GetType().FullName}");
+                        streamWriter.WriteLine($"Message: {exception.Message}");
+                        streamWriter.WriteLine($"StackTrace: {exception.StackTrace}");
+                    }
+                }
 
-		public static Logger Shared()
-		{
-			if (shared == null)
-			{
-				shared = new Logger();
-			}
+                // Write to console
+                TextWriter consoleWriter = priority == Priority.Error ? Console.Error : Console.Out;
+                consoleWriter.WriteLine(logEntry);
 
-			return shared;
-		}
+                if (exception != null) {
+                    consoleWriter.WriteLine($"Exception: {exception.GetType().FullName}");
+                    consoleWriter.WriteLine($"Message: {exception.Message}");
+                    consoleWriter.WriteLine($"StackTrace: {exception.StackTrace}");
+                }
+            }
+        }
 
-		public static void SetLogFile(string filename)
-		{
-			Logger.Shared().logFile = filename;
-		}
+        public static Logger Shared() {
+            return shared ??= new Logger();
+        }
 
-		public static void Log(string value)
-		{
-			Logger.Shared();
-		}
-	}
+        public static void SetLogFile(string filename) {
+            Shared()._logFile = filename;
+        }
+
+        public static void Log(string value) {
+            Shared().Log(Priority.Log, value);
+        }
+
+        public static void Error(string value, Exception exception = null) {
+            Shared().Log(Priority.Error, value, exception);
+        }
+
+        public static void Trace(string value, Exception exception = null) {
+            Shared().Log(Priority.Trace, value, exception);
+        }
+    }
 }
-
