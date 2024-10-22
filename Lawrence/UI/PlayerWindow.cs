be926bb197ada8dbeb98ebd0df2d9546359a3c76
bordplate <vetle@bordplate.no>
2024-10-22 11:26:31 +0200

    Add a player information window to the GUI.

---
+++ Lawrence/UI/PlayerWindow.cs
@@ -0,0 +1,38 @@
+using Terminal.Gui;
+
+namespace Lawrence.UI; 
+
+public class PlayerWindow: Window {
+    public PlayerWindow(string playerName) {
+        Title = playerName;
+
+        var player = Game.Game.Shared().FindPlayerByUsername(playerName);
+
+        var playerInfo = new Label() {
+            X = 2,
+            Y = 2,
+            Width = Dim.Fill(),
+            Height = Dim.Fill(),
+        };
+
+        playerInfo.Text = $"""
+                           Level: {player.Level().GetName()}
+
+                           X: {player.x}
+                           Y: {player.y}
+                           Z: {player.z}
+                           """;
+       
+        // Close button at bottom center
+        var closeButton = new Button("Close") {
+            X = Pos.Center(),
+            Y = Pos.Percent(100) - 1,
+        };
+        closeButton.Clicked += () => {
+            Remove(this);
+        };
+        
+        Add(playerInfo);
+        Add(closeButton);
+    }
+}
